4a843392d41942aa23b1aa7ac7ff51706c2619de
tigrdima <duys79yandex.ru>
2022-01-11 10:45:25 +0300

    2. Сокобан. Тестовое задание. [#75283 #247310]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,33 +3,40 @@ package ru.job4j.puzzle;
 public class Win {
 
     public static boolean check(int[][] board) {
-        return checkRow(board) || checkCell(board);
+        boolean rsl = false;
 
-    }
-
-    public static boolean checkRow(int[][] board) {
-       int b = 1;
-
-        for (int[] ints : board) {
-            for (int cell = 0; cell < ints.length - 1; cell++) {
-                if (ints[cell] == 1 && ints[cell + 1] == 1) {
-                    b++;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][i] == 1 && (checkRow(board, j) || (checkCell(board, j)))) {
+                    rsl = true;
+                    break;
                 }
             }
         }
-        return b == board.length;
+        return rsl;
     }
 
-    public static boolean checkCell(int[][] board) {
-        int b = 1;
+    public static boolean checkRow(int[][] board, int row) {
+        boolean rsl = true;
 
-        for (int row = 0; row < board.length - 1; row++) {
-            for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[row][cell] == 1 && board[row + 1][cell] == 1) {
-                    b++;
-                }
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[row][cell] != 1) {
+                rsl = false;
+                break;
             }
         }
-        return b == board[0].length;
+        return rsl;
+    }
+
+    public static boolean checkCell(int[][] board, int cell) {
+        boolean rsl = true;
+
+        for (int row = 0; row < board[cell].length; row++) {
+            if (board[row][cell] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
     }
 }
